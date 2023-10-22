f905c598e128783a34a9f14496d451ae88b2c7ab
jguneratne <59374962+jguneratne@users.noreply.github.com>
2023-10-21 21:58:28 -0400

    Create dropdown transition smoothly down

--- src/dropDown.js
+++ src/dropDown.js
@@ -15,6 +15,8 @@ export function showSubMenu() {
     dropDownContent.classList.toggle("dropdown-content--active");
     dropDownItems.forEach((item) => {
       item.classList.toggle("dropdown-item--active");
+      //   item.style.animation =
+      //     "slide-right 0.5s ease-in-out calc(60ms * var(--delay)) 1 forwards;";
     });
   });
 }
