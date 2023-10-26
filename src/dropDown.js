3afabd64fa0094cde36ee094d2d745d7ba238013
jguneratne <59374962+jguneratne@users.noreply.github.com>
2023-10-26 07:29:18 -0400

    Update dropDown ES6 module

--- src/dropDown.js
+++ src/dropDown.js
@@ -7,16 +7,16 @@ import {
 } from "./querySelectors";
 
 export function showSubMenu() {
-  dropDownBtn.addEventListener("pointerdown", () => {
-    console.log("click");
-    dropDownDiv.classList.toggle("dropdown-div--active");
-    dropDownBtn.classList.toggle("dropdown-btn--active");
-    dropDownCarat.classList.toggle("gal-arrow--active");
-    dropDownContent.classList.toggle("dropdown-content--active");
-    dropDownItems.forEach((item) => {
-      item.classList.toggle("dropdown-item--active");
-      //   item.style.animation =
-      //     "slide-right 0.5s ease-in-out calc(60ms * var(--delay)) 1 forwards;";
-    });
+  dropDownDiv.addEventListener("pointerdown", (e) => {
+    if (e.target === dropDownBtn) {
+      console.log("click");
+      dropDownDiv.classList.toggle("dropdown-div--active");
+      dropDownBtn.classList.toggle("dropdown-btn--active");
+      dropDownCarat.classList.toggle("gal-arrow--active");
+      dropDownContent.classList.toggle("dropdown-content--active");
+      dropDownItems.forEach((item) => {
+        item.classList.toggle("dropdown-item--active");
+      });
+    }
   });
 }
