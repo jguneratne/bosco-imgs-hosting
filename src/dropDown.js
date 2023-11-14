1864bd52eadbc4b9d29427d02c1c360ef43b786e
jguneratne <59374962+jguneratne@users.noreply.github.com>
2023-11-14 07:58:34 -0500

    Comment out console.log lines

--- src/dropDown.js
+++ src/dropDown.js
@@ -7,9 +7,9 @@ import {
 
 export function showSubMenu() {
   dropDownDiv.addEventListener("pointerdown", (e) => {
-    console.log(e.target);
+    // console.log(e.target);
     if (e.target.nodeName === "A" || e.target === dropDownCarat) {
-      console.log("click");
+      // console.log("click");
       dropDownCarat.classList.toggle("gal-arrow--active");
       dropDownContent.classList.toggle("dropdown-content--active");
       dropDownItems.forEach((item) => {
