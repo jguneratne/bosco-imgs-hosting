71e74445188ca55ba4dcf957f6052e62c0ed5da6
jguneratne <59374962+jguneratne@users.noreply.github.com>
2024-01-17 18:39:51 -0500

    Add stats rule under module.exports to see child compilation error

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  stats: { children: true },
   mode: "development",
   entry: [
     "./src/index.js",
