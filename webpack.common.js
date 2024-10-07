a7f355fc26ed4913cade0b5b24bf16c976f027f1
jguneratne <59374962+jguneratne@users.noreply.github.com>
2024-10-07 17:37:09 -0400

    Readd crossOriginLoading and integrety key to config

--- webpack.common.js
+++ webpack.common.js
@@ -14,7 +14,7 @@ module.exports = {
       // See babel documentation: https://github.com/babel/babel-loader
       arrowFunction: false,
     },
-    // crossOriginLoading: "anonymous", // required for subresource integrity to work See: https://dev.to/webdiscus/webpack-auto-generate-an-integrity-hash-for-link-and-script-tags-in-an-html-template-48p5
+    crossOriginLoading: "anonymous", // required for subresource integrity to work See: https://dev.to/webdiscus/webpack-auto-generate-an-integrity-hash-for-link-and-script-tags-in-an-html-template-48p5
   },
   resolve: {
     alias: {
@@ -83,7 +83,7 @@ module.exports = {
         // output filename for CSS
         filename: "styles/[name].[contenthash:8].css",
       },
-      //integrity: "auto", // include `integrity` attribute in production mode only
+      integrity: "auto", // include `integrity` attribute in production mode only
 
       preprocessor: "ejs",
       preprocessorOptions: {
